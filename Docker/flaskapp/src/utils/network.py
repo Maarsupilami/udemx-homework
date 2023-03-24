313daeb5074612616c35a2aef00bf570b8c6ae35
Maarsupilami <vajger.mark@outlook.hu>
2023-03-24 14:18:00 +0100

    Revert "Add some Docker files and project"
    This reverts commit d958dd07fb1b168c25fc30d947724aa44cc08fa4.

--- Docker/flaskapp/src/utils/network.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-def fetch_details():
-    try:
-        host_name = socket.gethostname()
-        host_ip = socket.gethostbyname(host_name)
-        return host_name, host_ip
-    except:
-        print("Unable toget Hostname and IP address!")
