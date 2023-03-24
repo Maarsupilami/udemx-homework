b4f429af793a827f7ef8de029b39f45000d14bb5
Maarsupilami <vajger.mark@outlook.hu>
2023-03-24 14:27:05 +0100

    Add simple flaskapp and dockerfile for it

---
+++ Docker/flaskapp/src/utils/network.py
@@ -0,0 +1,9 @@
+import socket
+
+def fetch_details():
+    try:
+        host_name = socket.gethostname()
+        host_ip = socket.gethostbyname(host_name)
+        return host_name, host_ip
+    except:
+        print("Unable toget Hostname and IP address!")
